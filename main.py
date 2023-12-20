f7e27b05b46383996902932f9c0ddb0fc32c6042
İkanaide <143414698+kosero@users.noreply.github.com>
2023-12-21 01:52:47 +0300

    asukacıklar

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='a!', intents=intents)
 TOKEN = "bot_token"
-liste = ["selam", "Evangelion", "Naber chat", "Selammm", "Selamm", "Selamlar", "Yapay zeka", "evangelion", "ii geceler", "ii uykular", "iyi geceler", "iyi uykular", "merhaba", "merhabalar", "nasılsınız", "sa.", "selammm", "selamlar", "slm", "sa", "ii gclr", "Neon Genesis Evangelion"]
+word_list = ["hello", "Evangelion", "Hi chat", "Helloo", "Hello", "Greetings", "evangelion", "good nights", "good sleeps", "hi.", "Neon Genesis Evangelion"]
 
 @bot.event
 async def on_ready():
@@ -18,30 +18,30 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    # Asuka ve Rei'ye özel tepkiler
-    if "asuka" in message.content.lower() and message.content.lower().index("asuka") == 0:
+    # Special reactions to Asuka and Rei
+    elif "asuka" in message.content.lower() and message.content.lower().index("asuka") == 0:
         await message.channel.typing()
         await reicikler.asukaspeak(message)
         return
     
-    if "<@bot_id>" in message.content.lower(): 
+    elif "<@bot_id>" in message.content.lower(): 
         await message.channel.typing()
         await reicikler.asukaspeak(message)
         return
         
-    if "rei" in message.content.lower() and message.content.lower().index("rei") == 0:
+    elif "rei" in message.content.lower() and message.content.lower().index("rei") == 0:
         await message.channel.typing()
         await reicikler.reispeak(message)
         return
 
-    # Liste içindeki kelimeleri kontrol etme
-    if any(word.lower() in message.content.lower() for word in liste):
+    # Checking the words in the list
+    elif any(word.lower() in message.content.lower() for word in liste):
         await message.channel.typing()
         await reicikler.asukaspeak(message)
         return
 
-    # Alıntı yapılan mesajın bot tarafından gönderilip gönderilmediğini kontrol etme
-    if message.reference and message.reference.message_id and message.reference.resolved.author.id == bot.user.id:
+    # check whether the reply message was sent by a bot or not
+    elif message.reference and message.reference.message_id and message.reference.resolved.author.id == bot.user.id:
         await message.channel.typing()
         await reicikler.asukaspeak(message)
         return
